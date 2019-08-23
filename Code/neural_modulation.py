761313dfb9288de63e8880daf38b2f7d445c0b41
yabernar <mad_otter@protonmail.com>
2019-08-23 15:23:04 +0200

    Saving files

--- Code/neural_modulation.py
+++ Code/neural_modulation.py
@@ -11,27 +11,27 @@ path = "/users/yabernar/GrosDisque/CDNET14"
 path2 = "/users/yabernar/workspace/aweSOM/Data/images/tracking/ducks/"
 
 categories = sorted(os.listdir(path + "/dataset"), key=str.lower)
-elements = sorted(os.listdir(path + "/dataset/" + categories[3]), key=str.lower)
+elements = sorted(os.listdir(path + "/dataset/" + categories[1]), key=str.lower)
 print(categories)
 print(elements)
 
-chosen_path = path + "/dataset/" + categories[3] + "/" + elements[2]
-temporal_ROI = (1, 3200)
+chosen_path = path + "/dataset/" + categories[1] + "/" + elements[0]
+temporal_ROI = (400, 1700)
 plot = None
-# bkg = Image.open(chosen_path + "/input/" + 'in{0:06d}.jpg'.format(1))
-bkg = Image.open(path2 + 'ducks{0:05d}.png'.format(1))
+bkg = Image.open(chosen_path + "/input/" + 'in{0:06d}.jpg'.format(472))
+# bkg = Image.open(path + 'ducks{0:05d}.png'.format(1))
 
 ############
 # LEARNING #
 ############
-pictures_dim = [10, 10]
+pictures_dim = [16, 16]
 parameters = Parameters({"pictures_dim": pictures_dim})
 data = MosaicImage(bkg, parameters)
-nb_epochs = 30
+nb_epochs = 50
 inputs_SOM = Parameters({"alpha": Variable(start=0.5, end=0.25, nb_steps=nb_epochs),
                          "sigma": Variable(start=0.1, end=0.03, nb_steps=nb_epochs),
                          "data": data.get_data(),
-                         "neurons_nbr": (20, 30),
+                         "neurons_nbr": (10, 10),
                          "epochs_nbr": nb_epochs})
 som = SOM(inputs_SOM)
 for i in range(nb_epochs):
@@ -73,9 +73,9 @@ plot = None
 for i in range(temporal_ROI[0], temporal_ROI[1]):
     i *= 1
     print('Image ', i)
-    # current = Image.open(chosen_path + "/input/in{0:06d}.jpg".format(i))
+    current = Image.open(chosen_path + "/input/in{0:06d}.jpg".format(i))
     truth = Image.open(chosen_path + "/groundtruth/gt{0:06d}.png".format(i))
-    current = Image.open(path2 + "ducks{0:05d}.png".format(i))
+    # current = Image.open(path2 + "ducks{0:05d}.png".format(i))
 
     bgs_difference = ImageChops.difference(bkg, current).convert('L')
 
@@ -83,8 +83,8 @@ for i in range(temporal_ROI[0], temporal_ROI[1]):
     som.set_data(new_data.get_data())
     winners = som.get_all_winners()
     diff_winners = np.zeros(winners.shape)
-    for i in range(len(winners)):
-        diff_winners[i] = manhattan_distance(np.asarray(winners[i]), np.asarray(initial_map[i]))
+    for j in range(len(winners)):
+        diff_winners[j] = manhattan_distance(np.asarray(winners[j]), np.asarray(initial_map[j]))
     # diff_winners = np.subtract(initial_map, winners)
     # diff_winners[diff_winners == 0] = -1
     # diff_winners = np.add(diff_winners, 1)
@@ -96,6 +96,9 @@ for i in range(temporal_ROI[0], temporal_ROI[1]):
     reconstructed = data.reconstruct(som.get_reconstructed_data(winners))
     som_difference = ImageChops.difference(Image.fromarray(reconstructed), current).convert('L')
     som_difference = ImageChops.multiply(som_difference, diff_winners)
+
+    som_difference.save("/users/yabernar/workspace/watSOM/Results/DNF/saliency"+str(i)+".png")
+#    diff_winners.save("/users/yabernar/workspace/watSOM/Results/diff_winners"+str(i)+".png")
 
     if plot is None:
         plot = []
